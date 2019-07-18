b634aa59346ba853af5a255ba4fc73961d6e1701
GitAntinus <gantinus@gmail.com>
2019-07-18 22:14:33 +0800

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/me/antinux/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package me.antinux.repository;
+
+import me.antinux.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/me/antinux/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package me.antinux.web.rest;
